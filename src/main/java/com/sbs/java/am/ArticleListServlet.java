5203e465998e62f45fd6733aeb0bccd3f931196f
woooyoung <axdsw121@gmail.com>
2021-09-13 12:53:17 +0900

    DB 연결

--- src/main/java/com/sbs/java/am/ArticleListServlet.java
+++ src/main/java/com/sbs/java/am/ArticleListServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/article/list")
+@WebServlet("/article/list") 
 public class ArticleListServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
