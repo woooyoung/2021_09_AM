c956bf7dbf87b23e64237a21bac4cbac8859770d
woooyoung <axdsw121@gmail.com>
2021-09-29 12:15:26 +0900

    회원 테이블 생성

--- db.sql
+++ db.sql
@@ -10,6 +10,14 @@ CREATE TABLE article(
     title CHAR(255) NOT NULL,
     `body` LONGTEXT NOT NULL
 );
+#회원 테이블 생성
+CREATE TABLE `member` (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    loginId CHAR(100) NOT NULL UNIQUE,
+    loginPw CHAR(100) NOT NULL,
+    `name` CHAR(100) NOT NULL
+);
 
 # 게시물 데이터 추가 
 INSERT INTO article 
