8004a0e535e12158d5255d91b26e8cca8fef87b0
woooyoung <axdsw121@gmail.com>
2021-10-06 12:41:54 +0900

    디스패처 서블릿과 컨트롤러 구조 도입

--- src/main/java/com/sbs/java/am/servlet/DispatcherServlet.java
+++ src/main/java/com/sbs/java/am/servlet/DispatcherServlet.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.List;
 import java.util.Map;
 
 import javax.servlet.ServletException;
@@ -15,16 +14,18 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.sbs.java.am.Config;
+import com.sbs.java.am.controller.ArticleController;
 import com.sbs.java.am.exception.SQLErrorException;
 import com.sbs.java.am.util.DBUtil;
 import com.sbs.java.am.util.SecSql;
 
-@WebServlet("/article/list")
-public class ArticleListServlet extends HttpServlet {
+@WebServlet("/s/*")
+public class DispatcherServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
 
 		// 커넥터 드라이버 활성화
@@ -44,7 +45,45 @@ public class ArticleListServlet extends HttpServlet {
 		try {
 			con = DriverManager.getConnection(Config.getDBUrl(), Config.getDBId(), Config.getDBPw());
 
-			
+			//모든 요청에 들어가기 전에 무조건 해줘야 하는 일
+			HttpSession session = request.getSession();
+
+			boolean isLogined = false;
+			int loginedMemberId = -1;
+			Map<String, Object> loginedMemberRow = null;
+
+			if (session.getAttribute("loginedMemberId") != null) {
+				loginedMemberId = (int) session.getAttribute("loginedMemberId");
+				isLogined = true;
+
+				SecSql sql = SecSql.from("SELECT * FROM `member`");
+				sql.append("WHERE id = ?", loginedMemberId);
+				loginedMemberRow = DBUtil.selectRow(con, sql);
+			}
+
+			request.setAttribute("isLogined", isLogined);
+			request.setAttribute("loginedMemberId", loginedMemberId);
+			request.setAttribute("loginedMemberRow", loginedMemberRow);
+			//모든 요청에 들어가기 전에 무조건 해줘야 하는 일
+
+			String requestUri = request.getRequestURI();
+			String[] requestUriBits = requestUri.split("/");
+
+			if (requestUriBits.length < 5) {
+				response.getWriter().append("올바른 요청이 아닙니다.");
+				return;
+			}
+
+			String controllerName = requestUriBits[3];
+			String actionMethodName = requestUriBits[4];
+
+			if (controllerName.equals("article")) {
+				ArticleController controller = new ArticleController(request, response, con);
+
+				if (actionMethodName.equals("list")) {
+					controller.actionList();
+				}
+			}
 
 		} catch (SQLException e) {
 			e.printStackTrace();
